b754845a1952e388b95361a5ac156fa6c39ba927
Andreia-Nunes <andreia.nunes@academico.ifpb.edu.br>
2023-09-13 16:24:46 -0300

    Resolvendo desafio 13

---
+++ src/Desafio13.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Desafio13 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        int ini = 5;
+        int fin = 10;
+
+        numeros.stream()
+                .filter(numero -> numero >= ini && numero <= fin)
+                .forEach(System.out::println);
+    }
+}
